9f47f708c7957e774e5db0d163a5b527dea656f8
Sergio Guzman <sealguzmanal@ittepic>
2023-02-15 22:20:41 -0700

    Appling finished web

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ComandosComponent } from './components/comandos/comandos.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'inicio', component : InicioComponent},
+  {path: 'comandos',component : ComandosComponent},
+  {path : '**', redirectTo: 'inicio',pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { ComandosComponent } from './components/comandos/comandos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    ComandosComponent
   ],
   imports: [
     BrowserModule,
